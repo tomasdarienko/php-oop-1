d8fe2b8c99569f985340c84fd62dd056f2e53949
tomasdarienko <darienkotomas@gmail.com>
2024-01-30 15:10:33 +0100

    definito un metodo

--- index.php
+++ index.php
@@ -7,27 +7,68 @@
 <?php
 
 
-     class movie
-     {
-          public $title;
-          public $genre;
-          public $director;
-          public $year;
+class movie
+{
+     public $title;
+     public $genre;
+     public $director;
+     public $year;
 
-          function __construct($title, $genre, $director, $year)
-          {
-               $this->title = $title;
-               $this->genre = $genre;
-               $this->director = $director;
-               $this->year = $year;
+     function __construct($title, $genre, $director, $year)
+     {
+          $this->title = $title;
+          $this->genre = $genre;
+          $this->director = $director;
+          $this->year = $year;
+     }
+
+
+     public function number2roman($num, $isUpper = true)
+     {
+          $n = intval($num);
+          $res = '';
+
+          /*** roman_numerals array ***/
+          $roman_numerals = array(
+               'M' => 1000,
+               'CM' => 900,
+               'D' => 500,
+               'CD' => 400,
+               'C' => 100,
+               'XC' => 90,
+               'L' => 50,
+               'XL' => 40,
+               'X' => 10,
+               'IX' => 9,
+               'V' => 5,
+               'IV' => 4,
+               'I' => 1
+          );
+
+          foreach ($roman_numerals as $roman => $number) {
+               /*** divide to get matches ***/
+               $matches = intval($n / $number);
+
+               /*** assign the roman char * $matches ***/
+               $res .= str_repeat($roman, $matches);
+
+               /*** substract from the number ***/
+               $n = $n % $number;
           }
 
-
+          /*** return the res ***/
+          if ($isUpper)
+               return $res;
+          else
+               return strtolower($res);
      }
-     $movie_1 = new movie("La maledizione della prima luna", " Avventura", "Gore Verbinski", "2003");
-     $movie_2 = new movie("Fast & Furious 7", " Azione", " James Wan", "2015");
-     $movie_3 = new movie("The Hateful Eight", " Western", " Quentin Tarantino", "2015");
-     $movie_4 = new movie("Babylon ", "Drammatico", "Damien Chazelle", "2022");
+
+}
+$movie_1 = new movie("La maledizione della prima luna", " Avventura", "Gore Verbinski", "2003");
+$movie_2 = new movie("Fast & Furious 7", " Azione", " James Wan", "2015");
+$movie_3 = new movie("The Hateful Eight", " Western", " Quentin Tarantino", "2015");
+$movie_4 = new movie("Babylon ", "Drammatico", "Damien Chazelle", "2022");
+
 ?>
 
 <!DOCTYPE html>
@@ -41,7 +82,71 @@
 
 <body>
 
+     <div>
+          <ul>
+               <li>
+                    <?php echo $movie_1->title; ?>
+                    <ul>
 
+                         <li>
+                              <?php echo $movie_1->genre; ?>
+                         </li>
+                         <li>
+                              <?php echo $movie_1->director; ?>
+                         </li>
+                         <li>
+                              <?php echo $movie_1->number2roman($movie_1->year); ?>
+                         </li>
+                    </ul>
+               </li>
+               <li>
+                    <?php echo $movie_2->title; ?>
+                    <ul>
+
+                         <li>
+                              <?php echo $movie_2->genre; ?>
+                         </li>
+                         <li>
+                              <?php echo $movie_2->director; ?>
+                         </li>
+                         <li>
+                              <?php echo $movie_2->number2roman($movie_2->year); ?>
+                         </li>
+                    </ul>
+               </li>
+               <li>
+                    <?php echo $movie_3->title; ?>
+                    <ul>
+
+                         <li>
+                              <?php echo $movie_3->genre; ?>
+                         </li>
+                         <li>
+                              <?php echo $movie_3->director; ?>
+                         </li>
+                         <li>
+                              <?php echo $movie_3->number2roman($movie_3->year); ?>
+                         </li>
+                    </ul>
+               </li>
+               <li>
+                    <?php echo $movie_4->title; ?>
+                    <ul>
+
+                         <li>
+                              <?php echo $movie_4->genre; ?>
+                         </li>
+                         <li>
+                              <?php echo $movie_4->director; ?>
+                         </li>
+                         <li>
+                              <?php echo $movie_4->number2roman($movie_4->year); ?>
+                         </li>
+                    </ul>
+               </li>
+          </ul>
+
+     </div>
 
 </body>
 
